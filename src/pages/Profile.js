a272e2b2fc55acde5349495a3493b0c7846e34ae
Sreeram <sreeramvin@gmail.com>
2022-02-19 19:52:34 +0530

    latest changes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import DisplayBooksInProfile from '../Components/DisplayBooksInProfile';
 import DisplayFinesInProfile from '../Components/DisplayFinesInProfile';
 import { auth, db } from '../firebase-config';
-
+import './Profile.css';
 const Profile = () => {
     const [user, setUser] = useState({});
     const [userData, setUserData] = useState({})
@@ -24,8 +24,8 @@ const Profile = () => {
         getData();
     }, [user])
     return (
-        <div>
-            <h2>My profile</h2>
+        <div className='profile'>
+            <h2>My Profile</h2>
             <h3>Name: {userData.name}</h3>
             <h4>Library id: {userData.uid}</h4>
             <p>Registered email: {userData.email}</p>
